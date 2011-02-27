fca27a3029998ee52be83b3e8c8e40de8e1a0d59
Colin Flanagan <cflanagan@nymag.com>
2011-02-27 14:27:30 -0500

    Working script engine bundle

--- src/main/java/cfm/slingscript/SlingScriptEngine.java
+++ src/main/java/cfm/slingscript/SlingScriptEngine.java
@@ -1,29 +1,37 @@
 package cfm.slingscript;
 
 import org.apache.sling.scripting.api.AbstractSlingScriptEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.script.*;
 import java.io.Reader;
 
 public class SlingScriptEngine extends AbstractSlingScriptEngine {
+    private static final Logger log = LoggerFactory.getLogger(SlingScriptEngine.class);
 
     public SlingScriptEngine(SlingScriptFactory factory) {
         super(factory);
+        log.info("Initializing script engine");
     }
 
     public Object eval(String script, ScriptContext context) throws ScriptException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        log.info("evaling {} {}", script, context);
+        return new Object();
     }
 
     public Object eval(Reader reader, ScriptContext context) throws ScriptException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        log.info("Initializing script engine {} {}", reader, context);
+        return new Object();
     }
 
     public Bindings createBindings() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        log.info("creating Bindings");
+        return new SimpleBindings();
     }
 
     public ScriptEngineFactory getFactory() {
-        return getFactory();
+        log.info("getting Factory");
+        return super.getFactory();
     }
 }
--- src/main/java/cfm/slingscript/SlingScriptFactory.java
+++ src/main/java/cfm/slingscript/SlingScriptFactory.java
@@ -1,43 +1,62 @@
 package cfm.slingscript;
 
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.scripting.api.AbstractScriptEngineFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.script.ScriptEngine;
 import java.util.Arrays;
 import java.util.List;
 
+
+@Component(metatype = false)
+@Service(value = javax.script.ScriptEngineFactory.class)
+//@Properties({     })
+
 public class SlingScriptFactory extends AbstractScriptEngineFactory {
 
     private final static String ENGINE_NAME = "Sling Scripting Engine";
     private final static String ENGINE_VERSION = "0.1";
-    private final static List MIME_TYPES = Arrays.asList("application/x-sling");
-    private final static List EXTENSIONS = Arrays.asList();
+    private final static List<String> MIME_TYPES = Arrays.asList("application/x-slingscript");
+    private final static List<String> EXTENSIONS = Arrays.asList("cfm");
+
+    private static final Logger log = LoggerFactory.getLogger(SlingScriptFactory.class);
 
     public String getEngineName() {
+        log.info("getEngineName called");
         return ENGINE_NAME;
     }
 
     public String getEngineVersion() {
+        log.info("getEngineVersion called");
         return ENGINE_VERSION;
     }
 
     public String getLanguageName() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        log.info("getLanguageName called");
+        return ENGINE_NAME;
     }
 
     public String getLanguageVersion() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        log.info("getLanguageVersion called");
+        return ENGINE_VERSION;
     }
 
     public List<String> getExtensions() {
+        log.info("getExtensions called");
         return EXTENSIONS;
     }
 
     public List<String> getMimeTypes() {
+        log.info("getMimeTypes called");
         return MIME_TYPES;
     }
 
     public ScriptEngine getScriptEngine() {
-        return new SlingScriptEngine(this) ;
+        log.info("getScriptEngine called");
+        return new SlingScriptEngine(this);
     }
 }
